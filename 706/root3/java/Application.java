bea8f279d2d2bdb8ee3ab9698c7473c2c2f51b34
Hullaballoonatic <Hullaballoonatic@gmail.com>
2018-08-26 17:39:30 -0500

    2.1

---
+++ 706/root3/java/Application.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Application {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (input.hasNextLine()) {
+            
+        }
+    }
+}
