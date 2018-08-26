ae76906e9503a424ddb3de5ac20d9a061d6fcf4d
Hullaballoonatic <Hullaballoonatic@gmail.com>
2018-08-25 20:49:50 -0500

    100, 10189 working, starting 706

--- 10189/src/main/java/Main.java
+++ 10189/src/main/java/Main.java
@@ -1,49 +1,67 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        final Scanner sc = new Scanner(System.in);
+    private final static Scanner input = new Scanner(System.in);
 
-        int fieldNum = 0;
-        int numRows = 0;
-        int numCols = 0;
-        int curRow = 0;
-        int curCol = 0;
-        char[][] minefield = new char[0][0];
+    private static int[][] minefield;
+    private final static int MINE = -10;
+    private static Boolean isMine(int r, int c) {
+        return minefield[r][c] < 0;
+    }
 
-        while (sc.hasNextLine()) {
-            if (curRow == numRows) {
-                if (numRows > 0) {
-                    printField(flagField(minefield));
+    private static int fieldNum = 0;
+
+    private static void incrementNeighbors(int r, int c) {
+        Boolean above = r > 0;
+        Boolean below = r < minefield.length - 1;
+        Boolean left = c > 0;
+        Boolean right = c < minefield[0].length - 1;
+        if (above) minefield[r - 1][c]++;
+        if (below) minefield[r + 1][c]++;
+        if (left) minefield[r][c - 1]++;
+        if (right) minefield[r][c + 1]++;
+        if (above && left) minefield[r - 1][c - 1]++;
+        if (above && right) minefield[r - 1][c + 1]++;
+        if (below && left) minefield[r + 1][c - 1]++;
+        if (below && right) minefield[r + 1][c + 1]++;
+    }
+
+    private static void printField() {
+        if (fieldNum != 0) {
+            System.out.println();
+        }
+        fieldNum++;
+        System.out.printf("Field #%d:\n", fieldNum);
+        for (int r = 0; r < minefield.length; r++) {
+            StringBuilder output = new StringBuilder();
+            for (int c = 0; c < minefield[0].length; c++) {
+                if (isMine(r,c)) {
+                    output.append('*');
+                } else {
+                    output.append(minefield[r][c]);
                 }
-                fieldNum++;
-                System.out.printf("\nField #%d:\n", fieldNum);
-                numRows = sc.nextInt();
-                numCols = sc.nextInt();
-                minefield = new char[numRows][numCols];
-                curRow = 0;
-            } else {
-                while (curCol < numCols) {
-                    minefield[curRow][curCol] = sc.findInLine(".").charAt(curCol);
-                    curCol++;
-                }
-                curRow++;
             }
+            System.out.println(output.toString());
         }
     }
 
-    private static char[][] flagField(char[][] minefield) {
-        //todo convert
-        return minefield;
-    }
-
-    private static void printField(char[][] minefield) {
-        for (char[] row : minefield) {
-            StringBuilder sb = new StringBuilder();
-            for (char col : row) {
-                sb.append(col);
+    public static void main(String[] args) {
+        while (input.hasNextLine()) {
+            if (input.hasNextInt()) {
+                minefield = new int[input.nextInt()][input.nextInt()];
+                input.nextLine();
+            } else if (minefield.length > 0 && minefield[0].length > 0) {
+                for(int r = 0; r < minefield.length; r++) {
+                    String line = input.nextLine();
+                    for(int c = 0; c < minefield[0].length; c++) {
+                        if(line.charAt(c) == '*') {
+                            minefield[r][c] = MINE;
+                            incrementNeighbors(r, c);
+                        }
+                    }
+                }
+                printField();
             }
-            System.out.println(sb.toString());
         }
     }
 }
