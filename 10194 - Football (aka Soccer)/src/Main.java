d10c37c3ea337338211dbc2e181cd03a43846bba
Hullaballoonatic <Hullaballoonatic@gmail.com>
2018-09-12 16:13:27 -0500

    up

--- 10194 - Football (aka Soccer)/src/Main.java
+++ 10194 - Football (aka Soccer)/src/Main.java
@@ -34,20 +34,17 @@ class Tournament {
             String[] tmp = in.nextLine().split("@");
             String teamAstr = tmp[0];
             String teamBstr = tmp[1];
+
             tmp = teamAstr.split("#");
             String teamAname = tmp[0];
             Team a = null;
             for (Team team : teams) {
                 if (team.name.equals(teamAname)) {
                     a = team;
-                    a.gamesPlayed++;
                     break;
                 }
             }
             int teamAgoals = Integer.parseInt(tmp[1]);
-
-            assert a != null;
-            a.goalsScored += teamAgoals;
 
             tmp = teamBstr.split("#");
             String teamBname = tmp[1];
@@ -55,34 +52,30 @@ class Tournament {
             for (Team team : teams) {
                 if (team.name.equals(teamBname)) {
                     b = team;
-                    b.gamesPlayed++;
                     break;
                 }
             }
             int teamBgoals = Integer.parseInt(tmp[0]);
 
             assert b != null;
+            assert a != null;
+            a.goalsScored += teamAgoals;
             b.goalsScored += teamBgoals;
-
             b.goalsAgainst += teamAgoals;
             a.goalsAgainst += teamBgoals;
 
             int goalDifference = teamAgoals - teamBgoals;
 
             if (goalDifference == 0) {
-                a.points++;
                 a.numTies++;
-                b.points++;
                 b.numTies++;
             } else if (goalDifference > 0) {
                 a.numWins++;
                 b.numLoss++;
-                a.points += 3;
                 a.highestGoalDifference = Math.max(a.highestGoalDifference, goalDifference);
             } else {
                 a.numLoss++;
                 b.numWins++;
-                b.points += 3;
                 b.highestGoalDifference = Math.max(a.highestGoalDifference, -1 * goalDifference);
             }
         }
@@ -97,16 +90,16 @@ class Tournament {
     static class TeamComparator implements Comparator<Team> {
         @Override
         public int compare(Team a, Team b) {
-            if (a.points > b.points) return 1;
-            if (a.points < b.points) return -1;
+            if (a.points() > b.points()) return 1;
+            if (a.points() < b.points()) return -1;
             if (a.numWins > b.numWins) return 1;
             if (a.numWins < b.numWins) return -1;
             if (a.highestGoalDifference > b.highestGoalDifference) return 1;
             if (a.highestGoalDifference < b.highestGoalDifference) return -1;
             if (a.goalsScored > b.goalsScored) return 1;
             if (a.goalsScored < b.goalsScored) return -1;
-            if (a.gamesPlayed < b.gamesPlayed) return 1;
-            if (a.gamesPlayed > b.gamesPlayed) return -1;
+            if (a.gamesPlayed() < b.gamesPlayed()) return 1;
+            if (a.gamesPlayed() > b.gamesPlayed()) return -1;
             String aName = a.name.toLowerCase();
             String bName = b.name.toLowerCase();
             int length = Math.min(aName.length(), bName.length());
@@ -120,14 +113,20 @@ class Tournament {
 
     class Team {
         String name;
-        int points = 0;
         int numWins = 0;
         int numTies = 0;
         int numLoss = 0;
         int goalsScored = 0;
         int goalsAgainst = 0;
         int highestGoalDifference;
-        int gamesPlayed;
+
+        int points() {
+            return 3 * numWins + numTies;
+        }
+
+        int gamesPlayed() {
+            return numWins+numTies+numLoss;
+        }
 
         Team(String name) {
             this.name = name;
@@ -140,8 +139,9 @@ class Tournament {
         for (int i = 1; i == numTeams; i++) {
             Team team = teams.pollFirst();
             assert team != null;
-            System.out.printf("%d) %s %dp, %dg (%d-%d-%d), %dgd (%d-%d)\n", i + 1, team.name, team.points, team.gamesPlayed, team.numWins, team.numTies, team.numLoss, team.highestGoalDifference, team.goalsScored, team.goalsAgainst);
+            System.out.printf("%d) %s %dp, %dg (%d-%d-%d), %dgd (%d-%d)\n",
+                    i + 1, team.name, team.points(), team.gamesPlayed(), team.numWins, team.numTies,
+                    team.numLoss, team.highestGoalDifference, team.goalsScored, team.goalsAgainst);
         }
     }
 }
-
