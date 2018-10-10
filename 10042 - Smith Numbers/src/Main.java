a6ed47de118eea255198441b6a0eb9cf62981412
Hullaballoonatic <Hullaballoonatic@gmail.com>
2018-10-10 11:30:24 -0500

    smith numbers... i think correct?

--- 10042 - Smith Numbers/src/Main.java
+++ 10042 - Smith Numbers/src/Main.java
@@ -1,21 +1,59 @@
 import java.util.*;
 
+import static java.lang.Math.sqrt;
 import static java.lang.System.out;
 
 class Main {
-    private static final Scanner in = new Scanner(System.in);
+    private static final Scanner in  = new Scanner(System.in);
+    private static final int     MAX = 1000000000;
 
     public static void main(String[] args) {
         int t = in.nextInt();
-        in.nextLine();
+
         while(t-->0) {
-            String tmp = in.nextLine();
-            int sum = 0, s = 0;
-            for (char c : tmp.toCharArray())
-                sum += (int) c;
-            int n = Integer.parseInt(tmp);
+            int s = in.nextInt();
+
+            while(true) {
+                s++;
+                String num = Integer.toString(s);
+                String primes = prime_digits(s);
+                if (!num.equals(primes) && sumDigits(num) == sumDigits(primes))
+                    break;
+            }
 
             out.println(s);
         }
     }
+
+    private static String prime_digits(int num) {
+        StringBuilder primeDigits = new StringBuilder();
+
+        // while number is even
+        while (num%2==0) {
+            num /= 2;
+            primeDigits.append(2);
+        }
+
+        // number is odd
+        for(int i = 3; i < sqrt(num); i += 2) {
+            while (num%i==0) {
+                num /= i;
+                primeDigits.append(i);
+            }
+        }
+
+        if(num > 2)
+            primeDigits.append(num);
+
+        return primeDigits.toString();
+    }
+
+    private static int sumDigits(String nums) {
+        int sum = 0;
+        for (char c : nums.toCharArray())
+            sum += c - '0';
+        return sum;
+    }
+
+
 }
